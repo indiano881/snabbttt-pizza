9887ca805971ca65f3d194aa2510198bd1beef8c
Davide Baldi <d.baldi88@gmail.com>
2024-04-12 09:42:33 +0200

    basic routing done-start to work on hedader and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   
 
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={"hello world"}/>
+        <Route path="order" element={"order"}/>
+      </Routes>
     </>
   )
 }
