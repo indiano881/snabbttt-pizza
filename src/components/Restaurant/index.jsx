f89286e2da1b10e9cacdbe6eafb3da6c6f7aca25
Davide Baldi <d.baldi88@gmail.com>
2024-04-12 10:49:55 +0200

    prestuled somhow

--- src/components/Restaurant/index.jsx
+++ src/components/Restaurant/index.jsx
@@ -5,11 +5,11 @@ import {getImageURL} from "../../utils/functions.js"
 function Restaurant({props}) {
     return (
         <div className={styles.container}>
-            <img src={getImageURL(props.image1)} alt={props.name} width={"200px"} height={"auto"}/>
+            <img  className={styles.image} src={getImageURL(props.image1)} alt={props.name} width={"200px"} height={"auto"}/>
             <h2>{props.name}</h2>
             <h4>{props.adress}</h4>
             <h4>{props.phone}</h4>
-            <img src={getImageURL(props.image2)} alt={props.name} width={"200px"} height={"auto"}/>
+            <img className={styles.image} src={getImageURL(props.image2)} alt={props.name} width={"200px"} height={"auto"}/>
         </div>
     )
 }
